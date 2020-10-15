8052a39e78d5834f3fc8b2eb3a27463f2f604fb5
Johnny Jefferson José Ramos <61524749+Johnny-J-Ramos@users.noreply.github.com>
2020-10-15 10:04:19 -0300

    finalidado os arquivos, só precisa de conferir o script

--- js/script.js
+++ js/script.js
@@ -22,13 +22,13 @@ function getFormattedNumber() {
   if (num == '-') {
     return '';
   }
-  var n = Number(num);
+  var n = number(num);
   var value = n.toLocaleString('en');
   return value;
 }
 
 function reverseNumberFormar(num) {
-  return Number(num.replace(/,/g, ''));
+  return number(num.replace(/,/g, ''));
 }
 var operator = document.getElementsByClassName('operator');
 for (var i = 0; i < operator.length; i++) {
@@ -47,7 +47,7 @@ for (var i = 0; i < operator.length; i++) {
       var history = getHistory();
       if (output == '' && history != '') {
         if (isNaN(history[history.length - 1])) {
-          history = history.substr(0, history - length - 1);
+          history = history.substr(0, history.length - 1);
         }
       }
       if (output != '' || history != '') {
@@ -71,7 +71,7 @@ var number = document.getElementsByClassName('number');
 for (var i = 0; i < number.length; i++) {
   number[i].addEventListener('click', function () {
     var output = reverseNumberFormar(getOutput());
-    if (ou != isNaN) {
+    if (output != NaN) {
       output = output + this.id;
       printOutput(output);
     }
